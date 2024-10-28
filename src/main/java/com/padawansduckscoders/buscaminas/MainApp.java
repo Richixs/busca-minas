728c8768f2013db42bdacad80643b8dd3550241c
Ricardo Ramirez Caero <ricardoramirezcaero123@gmail.com>
2024-10-28 16:58:01 -0400

    feat: menu background color #1B1B1B

--- src/main/java/com/padawansduckscoders/buscaminas/MainApp.java
+++ src/main/java/com/padawansduckscoders/buscaminas/MainApp.java
@@ -3,6 +3,7 @@ package com.padawansduckscoders.buscaminas;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,9 @@ import java.io.IOException;
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("view/buscaminas-menu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 620, 720);
+        stage.setTitle("Buscaminas");
         stage.setScene(scene);
         stage.show();
     }
