6c44b4141edc52d52870dafeb461d5a4a7d75a77
Ricardo Ramirez Caero <ricardoramirezcaero123@gmail.com>
2024-10-29 02:58:16 -0400

    feat: Play menu buttons

--- src/main/java/com/padawansduckscoders/buscaminas/MainApp.java
+++ src/main/java/com/padawansduckscoders/buscaminas/MainApp.java
@@ -3,6 +3,7 @@ package com.padawansduckscoders.buscaminas;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ import com.padawansduckscoders.buscaminas.controller.MenuController;
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Font.loadFont(getClass().getResourceAsStream("/fonts/Modak-Regular.ttf"), 36);
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("view/buscaminas-menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 620, 720);
         MenuController controller = fxmlLoader.getController();
--- src/main/java/com/padawansduckscoders/buscaminas/controller/MenuController.java
+++ src/main/java/com/padawansduckscoders/buscaminas/controller/MenuController.java
@@ -4,11 +4,17 @@ import javafx.application.HostServices;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 public class MenuController {
     @FXML
-    private Button buttonPlay, buttonReport, buttonExit;
+    private Button buttonPlay, buttonReport, buttonExit, buttonEasy, buttonNormal, buttonHard, buttonCustom, buttonBack, buttonStartGame;
+    @FXML
+    private VBox vboxMenuButtons, vboxPlayButtons;
+    @FXML
+    private HBox hboxPlayButtons;
     
     private HostServices hostServices;
 
@@ -20,7 +26,9 @@ public class MenuController {
 
     @FXML
     protected void onPlayButtonClick() {
-        System.out.println("Play");
+        vboxMenuButtons.setVisible(false);
+        vboxPlayButtons.setVisible(true);
+        hboxPlayButtons.setVisible(true);
     }
 
     @FXML
@@ -69,5 +77,109 @@ public class MenuController {
     protected void onExitMouseExited() {
         buttonExit.setStyle(BUTTONSTYLE);
         buttonExit.setTextFill(Color.valueOf("#ffffff"));
+    }
+
+    @FXML
+    protected void onEasyButtonClick() {
+        System.out.println("easy");
+    }
+
+    @FXML
+    protected void onEasyMouseEntered() {
+        buttonEasy.setStyle(BUTTONSTYLE + "-fx-background-color: #2C2C2C; -fx-border-color: #ff0000;");
+        buttonEasy.setTextFill(Color.valueOf("#ff0000"));
+    }
+
+    @FXML
+    protected void onEasyMouseExited() {
+        buttonEasy.setStyle(BUTTONSTYLE);
+        buttonEasy.setTextFill(Color.valueOf("#ffffff"));
+    }
+
+    @FXML
+    protected void onNormalButtonClick() {
+        System.out.println("normal");
+    }
+
+    @FXML
+    protected void onNormalMouseEntered() {
+        buttonNormal.setStyle(BUTTONSTYLE + "-fx-background-color: #2C2C2C; -fx-border-color: #ff0000;");
+        buttonNormal.setTextFill(Color.valueOf("#ff0000"));
+    }
+
+    @FXML
+    protected void onNormalMouseExited() {
+        buttonNormal.setStyle(BUTTONSTYLE);
+        buttonNormal.setTextFill(Color.valueOf("#ffffff"));
+    }
+
+    @FXML
+    protected void onHardButtonClick() {
+        System.out.println("hard");
+    }
+
+    @FXML
+    protected void onHardMouseEntered() {
+        buttonHard.setStyle(BUTTONSTYLE + "-fx-background-color: #2C2C2C; -fx-border-color: #ff0000;");
+        buttonHard.setTextFill(Color.valueOf("#ff0000"));
+    }
+
+    @FXML
+    protected void onHardMouseExited() {
+        buttonHard.setStyle(BUTTONSTYLE);
+        buttonHard.setTextFill(Color.valueOf("#ffffff"));
+    }
+
+    @FXML
+    protected void onCustomButtonClick() {
+        System.out.println("custom");
+    }
+
+    @FXML
+    protected void onCustomMouseEntered() {
+        buttonCustom.setStyle(BUTTONSTYLE + "-fx-background-color: #2C2C2C; -fx-border-color: #ff0000;");
+        buttonCustom.setTextFill(Color.valueOf("#ff0000"));
+    }
+
+    @FXML
+    protected void onCustomMouseExited() {
+        buttonCustom.setStyle(BUTTONSTYLE);
+        buttonCustom.setTextFill(Color.valueOf("#ffffff"));
+    }
+
+    @FXML
+    protected void onStartGameButtonClick() {
+        System.out.println("StartGame");
+    }
+
+    @FXML
+    protected void onStartGameMouseEntered() {
+        buttonStartGame.setStyle(BUTTONSTYLE + "-fx-background-color: #2C2C2C; -fx-border-color: #ff0000;");
+        buttonStartGame.setTextFill(Color.valueOf("#ff0000"));
+    }
+
+    @FXML
+    protected void onStartGameMouseExited() {
+        buttonStartGame.setStyle(BUTTONSTYLE);
+        buttonStartGame.setTextFill(Color.valueOf("#ffffff"));
+    }
+
+    @FXML
+    protected void onBackButtonClick() {
+        vboxMenuButtons.setVisible(true);
+        vboxPlayButtons.setVisible(false);
+        hboxPlayButtons.setVisible(false);
+    }
+
+    @FXML
+    protected void onBackMouseEntered() {
+        buttonBack.setStyle(BUTTONSTYLE + "-fx-background-color: #2C2C2C; -fx-border-color: #ff0000;");
+        buttonBack.setTextFill(Color.valueOf("#ff0000"));
+    }
+
+    @FXML
+    protected void onBackMouseExited() {
+        buttonBack.setStyle(BUTTONSTYLE);
+        buttonBack.setTextFill(Color.valueOf("#ffffff"));
     }
 }
